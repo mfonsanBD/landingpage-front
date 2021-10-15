bb332fea3984005eeadb573b72fb518e952521a0
mfonsanBD <iddeiasimplesmike@gmail.com>
2021-10-15 14:31:48 -0300

    correção no component de logotipo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { LogoProps } from 'types/api'
+import { MediaProps } from 'types/api'
 import { getImageUrl } from 'utils/getImageUrl'
 import * as S from './styles'
 
-const Logo = ({ alternativeText, url }: LogoProps) => (
+const Logo = ({ alternativeText, url }: MediaProps) => (
   <S.LogoWrapper src={getImageUrl(url)} alt={alternativeText} />
 )
 
